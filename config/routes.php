815931a3921751b5aac5156c6fba9f8e837bd045
Thomas Sihapanya <contact@tomsihap.fr>
2020-02-18 12:11:26 +0100

    Creating a room

--- config/routes.php
+++ config/routes.php
@@ -12,6 +12,8 @@ $router->get('', 'PagesController@index'); // Page d'accueil contenant entre aut
  * Routes ROOM
  */
 $router->get('/rooms/(\d+)', 'RoomsController@show'); // Affichage de 1 room
+$router->get('/rooms/new', 'RoomsController@new'); // Formulaire de création de room
+$router->post('/rooms', 'RoomsController@create'); // Traitement de la création de room
 
 /**
  * Routes CLIENT
--- src/controller/RoomsController.php
+++ src/controller/RoomsController.php
@@ -17,4 +17,24 @@ class RoomsController extends AbstractController {
             'room' => $room
         ]);
     }
+
+    /**
+     * Afficher le formulaire de craétion de room
+     * Route: GET /rooms/new
+     */
+    public function new()
+    {
+        echo $this->container->getTwig()->render('rooms/form.html.twig');
+    }
+
+    /**
+     * Traiter le formulaire de création de room
+     * Route: POST /rooms
+     */
+    public function create()
+    {
+        $this->container->getRoomManager()->create($_POST);
+
+        Header('Location: ' . $this->configuration['env']['base_path']);
+    }
 }
