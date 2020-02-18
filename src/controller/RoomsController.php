d36d2d720720f9ed59263a3af9182ba0000306f6
Thomas Sihapanya <contact@tomsihap.fr>
2020-02-18 12:36:23 +0100

    Loop client in select (not working now)

--- src/controller/RoomsController.php
+++ src/controller/RoomsController.php
@@ -9,12 +9,12 @@ class RoomsController extends AbstractController {
      * Route: GET /rooms/:id
      */
     public function show(int $id) {
-        // 1. Récupérer le car par son id
         $room = $this->container->getRoomManager()->findOneById($id);
 
-        //2. Afficher la room
+        $clients = $this->container->getClientManager()->findAll();
         echo $this->container->getTwig()->render('rooms/show.html.twig', [
-            'room' => $room
+            'room' => $room,
+            'clients' => $clients
         ]);
     }
 
